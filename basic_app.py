d6922b56b415540ea6d33f4d5d3c31774610bf3f
Exponential182 <mthomas4773@gmail.com>
2026-02-19 10:20:34 +1300

    Moved db to SQLAlchemy locattion, updated formatting on home.html. Made the SQLAlchemy actually work.

--- basic_app.py
+++ basic_app.py
@@ -1,53 +1,59 @@
-import sqlite3
+from flask import Flask, request, render_template
 
-from flask import Flask, request, render_template, g
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import (DeclarativeBase, Mapped, mapped_column,
+                            relationship)
+from sqlalchemy import String, Integer, ForeignKey, select
 
 app = Flask(__name__)
-DATABASE = "basic_db.db"
 
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+class Base(DeclarativeBase):
+    pass
 
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+class Engines(Base):
+    __tablename__ = "engine_maker"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    team_years: Mapped[list["Teams"]] = relationship(back_populates="engine")
 
 
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
+class Teams(Base):
+    __tablename__ = "team_performance"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    engine_id: Mapped[int] = mapped_column(ForeignKey("engine_maker.id"))
+    engine: Mapped[Engines] = relationship(back_populates="team_years")
+    year: Mapped[int] = mapped_column(Integer)
+    pos: Mapped[int] = mapped_column(Integer)
+    era: Mapped[String] = mapped_column(String)
+
+
+db = SQLAlchemy(model_class=Base)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///basic_db.db"
+db.init_app(app)
 
 
 @app.route("/")
 def home():
-    sql = """
-            SELECT Team_Performance.name, Engine_Maker.name, year, pos, era
-            FROM Team_Performance
-            JOIN Engine_Maker on Team_Performance.engine = Engine_Maker.id
-        """
-    results = query_db(sql)
+    results = db.session.execute(select(Teams)).scalars()
+    print(results)
     return render_template("home.html", data=results)
 
 
 @app.route("/year/<int:year>")
 def year(year):
-    sql = f"""
-            SELECT Team_Performance.name, Engine_Maker.name, year, pos, era
-            FROM Team_Performance
-            JOIN Engine_Maker on Team_Performance.engine = Engine_Maker.id
-            WHERE year = {year}
-        """
-    results = query_db(sql)
-    return render_template("year.html", data=results)
+    pass
+    # sql = f"""
+    #         SELECT Team_Performance.name, Engine_Maker.name, year, pos, era
+    #         FROM Team_Performance
+    #         JOIN Engine_Maker on Team_Performance.engine = Engine_Maker.id
+    #         WHERE year = {year}
+    #     """
+    # results = query_db(sql)
+    # return render_template("year.html", data=results)
 
 
 if __name__ == "__main__":
