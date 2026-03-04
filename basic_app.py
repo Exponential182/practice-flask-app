19f08915d26d506d87bb731a5112aa77ee63036a
Exponential182 <mthomas4773@gmail.com>
2026-03-05 10:12:26 +1300

    Setup Login system (still needs signout page, signup page), updated db to support argon2,  made login form

--- basic_app.py
+++ basic_app.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session, flash
 
-# DB
+# SQL Alchemy
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import (DeclarativeBase, Mapped, mapped_column,
                             relationship)
 from sqlalchemy import String, Integer, ForeignKey, select
+from sqlalchemy.exc import NoResultFound
 
 # Forms
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import (StringField, IntegerField, SubmitField, PasswordField,
+                     BooleanField)
 from wtforms.validators import DataRequired, Length, NumberRange
 
+# Passwords
+from flask_login import LoginManager, UserMixin, current_user, login_user
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "a-very-secret-secret-key"
+app.config["SECRET_KEY"] = """
+    e702bc4d1915c4735ca0c8cf8587674b92b9845d8e2705a931c741599b91b6a2
+"""
+login_manager = LoginManager()
+hasher = PasswordHasher(time_cost=3, parallelism=4, memory_cost=65536)
 
 
 # Tables
@@ -38,6 +49,17 @@ class Teams(Base):
     era: Mapped[str] = mapped_column(String)
 
 
+class Login(Base):
+    __tablename__ = "password_test_table"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String)
+    password: Mapped[str] = mapped_column(String)
+    admin: Mapped[bool] = mapped_column(Integer)
+
+
+db = SQLAlchemy(model_class=Base)
+
+
 # Forms
 class DataAdditionForm(FlaskForm):
     team_name = StringField("Team Name", validators=[
@@ -55,10 +77,38 @@ class DataAdditionForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
-db = SQLAlchemy(model_class=Base)
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[
+            DataRequired(), Length(min=4, max=50)
+        ]
+    )
+    password = PasswordField("Password", validators=[
+            DataRequired(), Length(min=8, max=100)
+        ]
+    )
+    remember = BooleanField("Remember Me?")
+    sumbit = SubmitField("Submit")
+
+
+# Flask-Login Architecture
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    statement = select(Login).limit(1).where(Login.id == int(user_id))
+    data = db.session.execute(statement)
+    data = data.scalar()
+    if data is None:
+        return None
+    return User(id=data.id)
+
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///basic_db.db"
 db.init_app(app)
+login_manager.init_app(app)
 
 
 @app.route("/")
@@ -88,6 +138,29 @@ def data_addition():
         db.session.commit()
         redirect("/home")
     return render_template("data_addition.html", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    WRONG_PASSWORD_MESSAGE = "Incorrect Password"
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        form_username = login_form.username.data
+        password = login_form.password.data
+        sql_statment = select(Login).where(Login.username == form_username)
+        try:
+            hash_info = db.session.execute(sql_statment.limit(1)).one()
+        except NoResultFound:
+            return redirect("/signup")
+        user_info: Login = hash_info[0]
+        try:
+            if hasher.verify(user_info.password, password):
+                print(login_form.remember.data)
+                login_user(User(str(user_info.id)), login_form.remember.data)
+                return redirect("/")
+        except VerifyMismatchError:
+            flash(WRONG_PASSWORD_MESSAGE)
+    return render_template("login.html", error=None, form=login_form)
 
 
 if __name__ == "__main__":
